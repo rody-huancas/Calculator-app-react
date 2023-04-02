fdca53953bc3f584e4d100676354c0e84ac771a1
Rody Huancas <rodyhuancas.04@gmail.com>
2023-04-01 19:15:35 -0500

    App

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Calculator } from "./components/calculator/Calculator";
+
+const App = () => {
+  return (
+    <>
+      <main className="main">
+        <h1 className="main__title">Calculator APP</h1>
+        <Calculator />
+      </main>
+    </>
+  );
+};
+
+export default App;
---
+++ src/components/calculator/Calculator.jsx
@@ -0,0 +1,105 @@
+import { useEffect, useRef, useState } from "react";
+import { btns, BTN_ACTIONS } from "./btnConfig";
+import "./calculator.css";
+
+export const Calculator = () => {
+  const btnRef = useRef(null);
+  const expRef = useRef(null);
+
+  const [expression, setExpression] = useState("");
+
+  useEffect(() => {
+    const btns = Array.from(btnRef.current.querySelectorAll("button"));
+
+    btns.forEach((e) => (e.style.height = e.offsetWidth + "px"));
+  }, []);
+
+  const btnClick = (item) => {
+    const expDiv = expRef.current;
+
+    if (item.action === BTN_ACTIONS.THEME)
+      document.body.classList.toggle("dark");
+
+    if (item.action === BTN_ACTIONS.ADD) {
+      addAnimSapan(item.display);
+
+      const oper = item.display !== "x" ? item.display : "*";
+      setExpression(expression + oper);
+    }
+
+    if (item.action === BTN_ACTIONS.DELETE) {
+      expDiv.parentNode.querySelector("div:last-child").innerHTML = "";
+      expDiv.innerHTML = "";
+
+      setExpression("");
+    }
+
+    if (item.action === BTN_ACTIONS.CALC) {
+      if (expression.trim().length <= 0) return;
+
+      expDiv.parentNode.querySelector("div:last-child").remove();
+
+      const cloneNode = expDiv.cloneNode(true);
+      expDiv.parentNode.appendChild(cloneNode);
+
+      const transform = `translateY(${
+        -(expDiv.offsetHeight + 5) + "px"
+      }) scale(0.4)`;
+
+      try {
+        let res = eval(expression);
+
+        setExpression(res.toString());
+        setTimeout(() => {
+          cloneNode.style.transform = transform;
+          expDiv.innerHTML = "";
+          addAnimSapan(Math.floor(res * 100000000) / 100000000);
+        }, 200);
+      } catch {
+        setTimeout(() => {
+          cloneNode.style.transform = transform;
+          expDiv.innerHTML = "";
+          expDiv.innerHTML = "Syntax Error";
+        }, 200);
+      }
+    }
+  };
+
+  const addAnimSapan = (content) => {
+    const expDiv = expRef.current;
+    const span = document.createElement("SPAN");
+
+    span.innerHTML = content;
+    span.style.opacity = 0;
+    expDiv.appendChild(span);
+
+    const width = span.offsetWidth + "px";
+    span.style.width = "0";
+
+    setTimeout(() => {
+      span.style.opacity = "1";
+      span.style.width = width;
+    }, 100);
+  };
+
+  return (
+    <div className="calculator">
+      <div className="calculator_result">
+        <div ref={expRef} className="calculator_result_exp"></div>
+        <div className="calculator_result_exp"></div>
+      </div>
+
+      <div ref={btnRef} className="calculator_btns">
+        {btns.map((item, index) => (
+          <button
+            key={index}
+            className={item.class}
+            onClick={() => btnClick(item)}
+          >
+            {item.display}
+          </button>
+        ))}
+      </div>
+    </div>
+  );
+};
